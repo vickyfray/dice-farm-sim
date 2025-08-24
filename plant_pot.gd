a496c70c5c5158fac2982b9ef3be51ea4a42d598
vickyfray <vickypurdue@hotmail.com>
2025-08-24 14:08:08 +0100

    Initial commit

---
+++ plant_pot.gd
@@ -0,0 +1,13 @@
+@tool
+extends TextureRect
+
+@export var plant_texture: Texture2D:
+	set(value):
+		plant_texture = value
+		if plant:
+			plant.texture = value
+@onready var plant = $Plant
+
+func _ready():
+	if plant and plant_texture:
+		plant.texture = plant_texture
