eff1bc310a235e6ef339ff9e77a9837b81a87f20
vickyfray <vickypurdue@hotmail.com>
2025-08-24 18:40:25 +0100

    pots are buttons

--- plant_pot.gd
+++ plant_pot.gd
@@ -6,7 +6,7 @@ extends TextureRect
 		plant_texture = value
 		if plant:
 			plant.texture = value
-@onready var plant = $PlantImage
+@onready var plant = $PlantButton
 
 func _ready():
 	if plant and plant_texture:
