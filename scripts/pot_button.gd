285b1618db881a7e540db96ee97d8b17a5b498a0
vickyfray <vickypurdue@hotmail.com>
2025-08-25 17:40:50 +0100

    moving some scripts and adding cursor images

---
+++ scripts/pot_button.gd
@@ -0,0 +1,5 @@
+extends TextureButton
+
+var cursor_plant = preload("res://assets/images/cursor-plant.png")
+var cursor_shears = preload("res://assets/images/cursor-shears.png")
+var cursor_watering = preload("res://assets/images/cursor-watering.png")
