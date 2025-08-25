68a00845c1c635f2f5d3806673dc5cb0877bd550
moohmGD <medwars@yahoo.co.uk>
2025-08-25 17:08:27 +0100

    Shop fixed

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var shopopen = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,7 +11,12 @@ func _process(delta):
 	pass
 
 
-func _on_shop_button_pressed():
-	print("Buttonpressed")
-	$Camera2D.position = Vector2(1050,360)
-	#580,360 default middle
+func _on_shop_button_toggled(toggled_on):
+	if toggled_on:
+		print("shopopened")
+		$Camera2D.position = Vector2(1050,360)
+		shopopen = true
+	if !toggled_on:
+		print("shop closed")
+		$Camera2D.position = Vector2(580,360)
+		shopopen  = false
